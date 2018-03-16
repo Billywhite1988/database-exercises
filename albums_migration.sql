2720c924c6c537a49f8e3f9881185ee97f6acaba
Billy <billy_white.88@yahoo.com>
2018-03-16 12:11:11 -0500

    Tables Ex

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,11 @@
+USE codeup_test_db;
 
 CREATE TABLE IF NOT EXISTS albums (
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  artist VARCHAR(512) NOT NULL,
-  name VARCHAR(512) NOT NULL,
-  release_date INT NOT NULL,
-  sales FLOAT NOT NULL,
-  genre CHAR(255) NOT NULL,
+  artist VARCHAR(120),
+  name VARCHAR(120),
+  release_date INT,
+  sales DECIMAL(6,2),
+  genre VARCHAR(305),
   PRIMARY KEY (id)
 );
