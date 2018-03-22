175ef223f8dc9225a9bd19d72d0557e24eb6facd
Billy <billy_white.88@yahoo.com>
2018-03-22 11:53:18 -0500

    Completed Subqueries Exercises

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -9,3 +9,23 @@ WHERE hire_date IN (
   select hire_date FROM employees
   WHERE emp_no = 101010
 );
+
+#Find all the titles held by all employees with the first name Aamod.
+#314 total titles, 6 unique titles
+
+SELECT title from titles
+WHERE emp_no IN (
+  SELECT emp_no FROM employees
+  WHERE first_name = 'Aamod'
+);
+
+#Find all the current department managers that are female.
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+  SELECT EMP_NO from DEPT_MANAGER
+  WHERE to_date > CURDATE()
+  AND gender = 'F'
+);
+
