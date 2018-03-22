ea0426179afb0f25524048c77200460214ade472
Billy <billy_white.88@yahoo.com>
2018-03-22 10:47:39 -0500

    Update on Relationship Exercises

--- relationship_drill.sql
+++ relationship_drill.sql
@@ -30,12 +30,34 @@ FROM dept_manager
 WHERE DEPT_NO IN (SELECT dept_no FROM departments WHERE dept_name = 'Sales')
 AND to_date > NOW();
 
-
-
-
-
-
 -- get all senior engineers in customer service department
+
+DESCRIBE titles;
+SELECT * from departments;
+
+SELECT * FROM titles
+WHERE title = 'Senior Engineer' AND emp_no IN (
+SELECT emp_no
+FROM dept_emp
+WHERE dept_no IN (
+
+SELECT dept_no
+FROM departments
+WHERE dept_name = 'Customer Service'
+  )
+);
+
 -- get all first and last names of current Sales department employees
+
+SELECT CONCAT_WS(' ' e.first_name, e.last_name)
+FROM employees e
+WHERE DEPT_NO IN (SELECT dept_no FROM departments WHERE dept_name = 'Sales')
+      AND emp.to_date > NOW();
+
+-- find first name and last of all department managers
+
+
+
+
 -- get the name of the employee with the highest salary
 
